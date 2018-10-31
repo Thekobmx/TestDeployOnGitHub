c6b6bb8dbd37aa60634f0bddfdb5b7950ca22bdd
Thekob <thekob.mx@gmail.com>
2018-10-31 21:44:27 +0700

    Initial Commit

---
+++ TestDeployGitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestDeployGitHub
+//
+//  Created by kotchaphan on 31/10/2561 BE.
+//  Copyright © 2561 Thanapat.P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
